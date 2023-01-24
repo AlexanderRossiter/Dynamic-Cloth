89638c4e7c994abce28b16f9a769adfa186830af
Alex Rossiter <alexrossiter28@gmail.com>
2023-01-24 19:26:34 +0000

    Delete build directory

--- build/src/util/CMakeFiles/util.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/util.dir/util.cpp.o"
-  "CMakeFiles/util.dir/util.cpp.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/util.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
